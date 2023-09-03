bb1a896025e5b080d71530687e7282510db89259
RolyMorvern <141121305+RolyMorvern@users.noreply.github.com>
2023-09-03 16:16:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+	$("#nav-toggle").click(() => {
+		$(".mobile-nav-container").toggle(100);
+		if ($(".mobile-nav-container").css("display") == "block") {
+			$("body").css("overflow", "hidden");
+		} else if ($(".mobile-nav-container").css("display") == "none") {
+			$("body").css("overflow", "visible");
+		}
+	})
+	$(".moile-nav-item").click(() => {
+		$(".mobile-nav-container").hide();
+		$("body").css("overflow", "visible");
+	})
+})
